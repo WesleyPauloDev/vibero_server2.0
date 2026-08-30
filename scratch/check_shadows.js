2c32d61d2cdc269c60523816290e9d56bc2280d5
WesleyPauloDev <wesleyoares7@gmail.com>
2026-08-30 03:57:14 -0300

    doram

--- scratch/check_shadows.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('db/re/item_db_equip.yml', 'utf8');
-const ids = [24001, 24002, 24004, 24005, 24666, 24667];
-for (const id of ids) {
-  const reg = new RegExp(`-\\s*Id:\\s*${id}[\\s\\S]*?Name:\\s*([^\\r\\n]+)`, 'g');
-  const m = reg.exec(content);
-  if (m) console.log(id, m[1]);
-}
--- scratch/query_sr.sql
+++
@@ -1,4 +0,0 @@
-
-SELECT id, nameid, refine, attribute, card0, card1, card2, card3, equip FROM inventory WHERE char_id = 150057 AND equip > 0 ORDER BY equip ASC;
-SELECT * FROM char_reg_num WHERE char_id = 150057;
-SELECT char_id, name, class, base_level, job_level, str, agi, vit, `int`, dex, luk, max_hp, max_sp FROM `char` WHERE char_id = 150057;
